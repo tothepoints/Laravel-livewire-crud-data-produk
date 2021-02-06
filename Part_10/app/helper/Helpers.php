bd49653ddf2553db5573f0050b810eab2cd57603
tothepoints <77325052+tothepoints@users.noreply.github.com>
2021-02-06 20:52:32 +0700

    Create Helpers.php

---
+++ Part_10/app/helper/Helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!function_exists('currency_IDR')) {
+   function currency_IDR($value)
+   {
+      return "Rp " . number_format($value, 0, ',', '.');
+   }
+}
+
+if (!function_exists('currencyIDRToNumeric')) {
+   function currencyIDRToNumeric($value)
+   {
+      return preg_replace('/\D/', '', $value);
+   }
+}
+
