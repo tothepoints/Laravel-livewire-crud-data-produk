5e66baadf1b6f20aec210d008436758093afaf49
tothepoints <77325052+tothepoints@users.noreply.github.com>
2021-02-10 19:45:16 +0700

    Create index-product.blade.php

---
+++ Part_14/resources/views/livewire/products/index-product.blade.php
@@ -0,0 +1,6 @@
+<div class="row">
+   <livewire:products.show-product />
+   <livewire:products.create-product />
+   <livewire:products.update-product />
+   <livewire:products.delete-product />
+</div>
